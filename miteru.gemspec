dda5023e022feeb8359a693184c0ca07d4468112
Manabu Niseki <manabu.niseki@gmail.com>
2018-09-17 15:40:00 +0900

    chore: remove unnecessary dependenies & spec code

--- miteru.gemspec
+++ miteru.gemspec
@@ -26,8 +26,6 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "bundler", "~> 1.16"
   spec.add_development_dependency "coveralls", "~> 0.8"
-  spec.add_development_dependency "dotenv", "~> 2.5"
-  spec.add_development_dependency "glint", "~> 0.1"
   spec.add_development_dependency "rake", "~> 12.3"
   spec.add_development_dependency "rspec", "~> 3.8"
   spec.add_development_dependency "vcr", "~> 4.0"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,6 @@
 require "bundler/setup"
 require "miteru"
 
-require "glint"
 require "vcr"
 require "webrick"
 
@@ -32,44 +31,6 @@ RSpec.configure do |config|
     end
     result
   end
-end
-
-def server
-  server = Glint::Server.new do |port|
-    http = WEBrick::HTTPServer.new(
-      BindAddress: "0.0.0.0",
-      Port: port,
-      Logger: WEBrick::Log.new(File.open(File::NULL, "w")),
-      AccessLog: []
-    )
-    http.mount_proc("/admin.asp") do |_, res|
-      body = "test"
-
-      res.status = 200
-      res.content_length = body.size
-      res.content_type = 'text/plain'
-      res.body = body
-    end
-    trap(:INT) { http.shutdown }
-    trap(:TERM) { http.shutdown }
-    http.start
-  end
-  Glint::Server.info[:http_server] = {
-    host: "0.0.0.0",
-    port: server.port
-  }
-  server
-end
-
-RSpec.shared_context "http_server" do
-  before(:all) {
-    @server = server
-    @server.start
-  }
-  after(:all) { @server.stop }
-
-  let(:host) { "0.0.0.0" }
-  let(:port) { @server.port }
 end
 
 VCR.configure do |config|
