76381bb0da0ad837e4e38684a532036f851772cc
Manabu Niseki <manabu.niseki@gmail.com>
2021-08-29 08:00:43 +0900

    refactor: renew CLI descriptions [skip ci]

--- lib/miteru/cli.rb
+++ lib/miteru/cli.rb
@@ -5,11 +5,11 @@ require "thor"
 module Miteru
   class CLI < Thor
     method_option :auto_download, type: :boolean, default: false, desc: "Enable or disable auto-download of phishing kits"
-    method_option :ayashige, type: :boolean, default: false, desc: "Enable or disable ayashige(ninoseki/ayashige) feed"
+    method_option :ayashige, type: :boolean, default: false, desc: "Enable or disable Ayashige(ninoseki/ayashige) feed"
     method_option :directory_traveling, type: :boolean, default: false, desc: "Enable or disable directory traveling"
-    method_option :download_to, type: :string, default: "/tmp", desc: "Directory to download file(s)"
-    method_option :post_to_slack, type: :boolean, default: false, desc: "Post a message to Slack if it detects a phishing kit"
-    method_option :size, type: :numeric, default: 100, desc: "Number of urlscan.io's results. (Max: 10,000)"
+    method_option :download_to, type: :string, default: "/tmp", desc: "Directory to download phishing kits"
+    method_option :post_to_slack, type: :boolean, default: false, desc: "Enable or disable Slack notification"
+    method_option :size, type: :numeric, default: 100, desc: "Number of urlscan.io's results to fetch. (Max: 10,000)"
     method_option :threads, type: :numeric, desc: "Number of threads to use"
     method_option :verbose, type: :boolean, default: true
     desc "execute", "Execute the crawler"
