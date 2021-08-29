68dc6d7d29cfa2cd88557f1d1715d34e3957ea36
Manabu Niseki <manabu.niseki@gmail.com>
2021-08-29 10:59:21 +0900

    refactor: use Configuration to get/set Slack settings

--- lib/miteru/notifier.rb
+++ lib/miteru/notifier.rb
@@ -9,8 +9,8 @@ module Miteru
       attachement = Attachement.new(url)
       kits = kits.select(&:filesize)
 
-      if post_to_slack? && kits.any?
-        notifier = Slack::Notifier.new(slack_webhook_url, channel: slack_channel)
+      if notifiable? && kits.any?
+        notifier = Slack::Notifier.new(Miteru.configuration.slack_webhook_url, channel: Miteru.configuration.slack_channel)
         notifier.post(text: message.capitalize, attachments: attachement.to_a)
       end
 
@@ -18,20 +18,8 @@ module Miteru
       puts "#{url}: #{message}"
     end
 
-    def post_to_slack?
-      slack_webhook_url? && Miteru.configuration.post_to_slack?
-    end
-
-    def slack_webhook_url
-      ENV.fetch "SLACK_WEBHOOK_URL"
-    end
-
-    def slack_channel
-      ENV.fetch "SLACK_CHANNEL", "#general"
-    end
-
-    def slack_webhook_url?
-      ENV.key? "SLACK_WEBHOOK_URL"
+    def notifiable?
+      Miteru.configuration.slack_webhook_url? && Miteru.configuration.post_to_slack?
     end
   end
 end
